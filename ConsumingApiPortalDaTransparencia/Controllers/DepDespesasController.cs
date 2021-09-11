b6b1022a115d0a16c156ae2961316bf7faf41363
Thiago da Silva Moreira <thiagosm.004@gmail.com>
2021-09-11 20:47:32 -0300

    creating the class DeputadosDespesas

---
+++ ConsumingApiPortalDaTransparencia/Controllers/DepDespesasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumingApiPortalDaTransparencia.Controllers
+{
+    public class DepDespesasController : Controller
+    {
+        // GET: DepDespesas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsumingApiPortalDaTransparencia/Models/DeputadosDespesas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsumingApiPortalDaTransparencia.Models
+{
+    public class DeputadosDespesas
+    {
+        public int ano { get; set; }
+        public string cnpjCpfFornecedor { get; set; }
+        public int codDocumento { get; set; }
+        public int codLote { get; set; }
+        public int codTipoDocumento { get; set; }
+        public string dataDocumento { get; set; }
+        public int mes { get; set; }
+        public string nomeFornecedor { get; set; }
+        public string numDocumento { get; set; }
+        public string numRessarcimento { get; set; }
+        public int parcela { get; set; }
+        public string tipoDespesa { get; set; }
+        public string tipoDocumento { get; set; }
+        public string urlDocumento { get; set; }
+        public int valorDocumento { get; set; }
+        public int valorGlosa { get; set; }
+        public int valorLiquido { get; set; }
+    }
+}
